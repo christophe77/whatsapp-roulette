5e6f94c6b8f14a2dfec32439322842f2401b2a81
christophe bellec <christophe.bellec@ekim.fr>
2022-03-21 09:23:10 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Header from './Header/Header';
-import RandomPhone from './RandomPhone/RandomPhone';
+import Header from "./Header/Header";
+import RandomPhone from "./RandomPhone/RandomPhone";
 
 function App() {
+
   return (
     <>
-     <Header />
-     <RandomPhone />
+      <Header />
+      <RandomPhone />
     </>
   );
 }
